971de2422510e5c0f4c15bf15c9d17b2a00e8715
Tomáš Vesecký <vesecky@proton.me>
2026-05-14 23:56:53 +0200

    Add dev shell definition

---
+++ flake.nix
@@ -0,0 +1,98 @@
+{
+  description = "A Nix-flake-based Python development environment";
+
+  inputs.nixpkgs.url = "nixpkgs/nixos-unstable"; # unstable Nixpkgs
+
+  outputs = {self, ...} @ inputs: let
+    inherit (inputs.nixpkgs) lib;
+
+    supportedSystems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "aarch64-darwin"
+    ];
+
+    forEachSupportedSystem = f:
+      lib.genAttrs supportedSystems (
+        system:
+          f {
+            inherit system;
+            pkgs = import inputs.nixpkgs {inherit system;};
+          }
+      );
+
+    /*
+    Change this value ({major}.{min}) to
+    update the Python virtual-environment
+    version. When you do this, make sure
+    to delete the `.venv` directory to
+    have the hook rebuild it for the new
+    version, since it won't overwrite an
+    existing one. After this, reload the
+    development shell to rebuild it.
+    You'll see a warning asking you to
+    do this when version mismatches are
+    present. For safety, removal should
+    be a manual step, even if trivial.
+    */
+    version = "3.13";
+  in {
+    devShells = forEachSupportedSystem (
+      {
+        pkgs,
+        system,
+      }: let
+        concatMajorMinor = v:
+          lib.pipe v [
+            lib.versions.splitVersion
+            (lib.sublist 0 2)
+            lib.concatStrings
+          ];
+
+        python = pkgs."python${concatMajorMinor version}";
+      in {
+        default = pkgs.mkShellNoCC {
+          venvDir = ".venv";
+
+          postShellHook = ''
+            venvVersionWarn() {
+            	local venvVersion
+            	venvVersion="$("$venvDir/bin/python" -c 'import platform; print(platform.python_version())')"
+
+            	[[ "$venvVersion" == "${python.version}" ]] && return
+
+            	cat <<EOF
+            Warning: Python version mismatch: [$venvVersion (venv)] != [${python.version}]
+                     Delete '$venvDir' and reload to rebuild for version ${python.version}
+            EOF
+            }
+
+            venvVersionWarn
+          '';
+
+          packages =
+            (with python.pkgs; [
+              venvShellHook
+              pip
+              numpy
+              pandas
+
+              # Add whatever else you'd like here.
+              # pkgs.basedpyright
+
+              # pkgs.black
+              # or
+              # python.pkgs.black
+
+              # pkgs.ruff
+              # or
+              # python.pkgs.ruff
+            ])
+            ++ [self.formatter.${system}];
+        };
+      }
+    );
+
+    formatter = forEachSupportedSystem ({pkgs, ...}: pkgs.nixfmt);
+  };
+}
